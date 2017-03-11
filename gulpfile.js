84a4b3d84a652602033b6eb400c2dfdd7f27d166
Pavel Ivonin <pavelivonin@yandex.ru>
2017-03-11 18:14:13 +0200

    Start

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp=require("gulp"),
+		browserSync=require("browser-sync"),
+		sass=require('gulp-sass');
+var SASS={
+		"SRC":'app/scss/**/*.scss',
+		"DEST":'app/css'
+		};
+gulp.task('server', function(){
+	browserSync({
+		port: 9000,
+		server: {
+			baseDir: 'app'
+		}
+	});
+});
+
+gulp.task('watch',function(){
+	gulp.watch([
+		'app/*.html',
+		'app/js/**/*.js',
+		'app/css/**/*.css',
+		'app/scss/**/*.scss'
+	],['sass']).on('change',browserSync.reload);
+});
+gulp.task('sass', function () {
+  return gulp.src(SASS.SRC)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest(SASS.DEST));
+});
+
+gulp.task('default', ['server','watch']);
